c1e4cecb5d68ef7cb83547499720421cdde5ee8b
emp233 <emp233@msn.com>
2016-07-24 19:51:38 -0500

    Crea el POJO de Animal de compañía

--- app/src/main/java/mx/evisoft/petagram/AnimalCompania.java
+++ app/src/main/java/mx/evisoft/petagram/AnimalCompania.java
@@ -4,4 +4,38 @@ package mx.evisoft.petagram;
  * Created by Evana Margain Puig on 24/07/16.
  */
 public class AnimalCompania {
+
+    private String nombre;
+    private int numeroLikes;
+    private int foto;
+
+    public AnimalCompania(String nombre, int numeroLikes, int foto) {
+        this.nombre = nombre;
+        this.numeroLikes = numeroLikes;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroLikes() {
+        return numeroLikes;
+    }
+
+    public void setNumeroLikes(int numeroLikes) {
+        this.numeroLikes = numeroLikes;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
 }
