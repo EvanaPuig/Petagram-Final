c98a844c05b17f116ef7807bb3866f5681bc9481
emp233 <emp233@msn.com>
2016-10-23 19:38:44 -0500

    Añade funcionalidad para mandar mail.

--- app/src/main/java/mx/evisoft/petagram/Activities/ContactoActivity.java
+++ app/src/main/java/mx/evisoft/petagram/Activities/ContactoActivity.java
@@ -2,14 +2,61 @@ package mx.evisoft.petagram.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import mx.evisoft.petagram.R;
+import mx.evisoft.petagram.Utils.SendMail;
 
-public class ContactoActivity extends AppCompatActivity {
+public class ContactoActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Declaring EditText
+    private EditText editNombreCompleto;
+    private EditText editEmail;
+    private EditText editDescripcion;
+
+    //Send button
+    private Button btnSiguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Initializing the views
+        editNombreCompleto = (EditText) findViewById(R.id.editNombreCompleto);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editDescripcion = (EditText) findViewById(R.id.editDescripcion);
+
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+
+        //Adding click listener
+        btnSiguiente.setOnClickListener(this);
     }
+
+    private void sendEmail() {
+        //Getting content for email
+        String email = editEmail.getText().toString().trim();
+        String subject = editNombreCompleto.getText().toString().trim();
+        String message = editDescripcion.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendEmail();
+    }
+
 }
---
+++ app/src/main/java/mx/evisoft/petagram/Utils/Config.java
@@ -0,0 +1,9 @@
+package mx.evisoft.petagram.Utils;
+
+/**
+ * Created by Evana Margain Puig on 23/10/16.
+ */
+public class Config {
+    public static final String EMAIL ="XXXXX";
+    public static final String PASSWORD ="XXXXXX";
+}
