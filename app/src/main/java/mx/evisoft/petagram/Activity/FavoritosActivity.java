02f966da9f624ce9df5e06f0891b34828d15e063
Evana Margain Puig <evana.margain@aeris.net>
2017-08-10 22:03:14 -0500

    Fixes favoritos screen

--- app/src/main/java/mx/evisoft/petagram/Activity/FavoritosActivity.java
+++ app/src/main/java/mx/evisoft/petagram/Activity/FavoritosActivity.java
@@ -9,6 +9,13 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -16,6 +23,12 @@ import mx.evisoft.petagram.R;
 import mx.evisoft.petagram.pojo.AnimalCompania;
 import mx.evisoft.petagram.adapter.AnimalCompaniaAdaptador;
 import mx.evisoft.petagram.db.BaseDatos;
+import mx.evisoft.petagram.restApi.EndpointsApi;
+import mx.evisoft.petagram.restApi.adapter.RestApiAdapter;
+import mx.evisoft.petagram.restApi.model.AnimalCompaniaResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FavoritosActivity extends AppCompatActivity {
 
@@ -42,26 +55,66 @@ public class FavoritosActivity extends AppCompatActivity {
 
         listaAnimalesCompania.setLayoutManager(llm);
 
-        inicializarListaAnimalesCompania();
-        inicializarAdaptador();
+        animalesCompania = new ArrayList<AnimalCompania>();
+
+        //inicializarListaAnimalesCompania();
+        obtenerMediosRecientes();
     }
 
-    public void inicializarAdaptador(){
-        AnimalCompaniaAdaptador adaptador = new AnimalCompaniaAdaptador(animalesCompania, this);
-        listaAnimalesCompania.setAdapter(adaptador);
-    }
+
 
     public void inicializarListaAnimalesCompania(){
-        animalesCompania = new ArrayList<AnimalCompania>();
+
 
         /*animalesCompania.add(new AnimalCompania("Huevo", "5", R.drawable.perro2));
         animalesCompania.add(new AnimalCompania("Sally", "3", R.drawable.perro5));
         animalesCompania.add(new AnimalCompania("Fluffy", "3", R.drawable.perro6));
         animalesCompania.add(new AnimalCompania("Pluto", "3", R.drawable.perro7));
-        animalesCompania.add(new AnimalCompania("Doggy", "3", R.drawable.perro8));*/
+        animalesCompania.add(new AnimalCompania("Doggy", "3", R.drawable.perro8));
 
         BaseDatos db = new BaseDatos(this);
 
-        animalesCompania = db.obtenerFavoritos();
+        animalesCompania = db.obtenerFavoritos();*/
+    }
+
+    public void obtenerMediosRecientes(){
+        RestApiAdapter restApiAdapter = new RestApiAdapter();
+        Gson gsonMediaRecent = restApiAdapter.construyeGsonDeserializadorMediaRecent();
+        EndpointsApi endpointsApi = restApiAdapter.establecerConexionRestApiInstagram(gsonMediaRecent);
+
+        Call<AnimalCompaniaResponse> animalCompaniaResponseCall = endpointsApi.getRecentMedia();
+
+
+
+        animalCompaniaResponseCall.enqueue(new Callback<AnimalCompaniaResponse>() {
+            @Override
+            public void onResponse(Call<AnimalCompaniaResponse> call, Response<AnimalCompaniaResponse> response) {
+                AnimalCompaniaResponse animalCompaniaResponse = response.body();
+                animalesCompania = animalCompaniaResponse.getAnimalesCompania();
+
+                mostrarAnimalesCompania();
+            }
+
+            @Override
+            public void onFailure(Call<AnimalCompaniaResponse> call, Throwable t) {
+
+                Toast.makeText(FavoritosActivity.this, "¡Algo pasó en la conexión! Intenta de nuevo", Toast.LENGTH_LONG).show();
+                Log.e("Favoritos", "Falló la conexión " + t.toString());
+            }
+        });
+    }
+
+    public void mostrarAnimalesCompania() {
+        ArrayList<AnimalCompania> animalesCompania2 = new ArrayList<AnimalCompania>(animalesCompania.subList(0, 5));
+        inicializarAdaptador(crearAdaptador(animalesCompania2));
+    }
+
+    public void inicializarAdaptador(AnimalCompaniaAdaptador adaptador){
+        listaAnimalesCompania.setAdapter(adaptador);
+    }
+
+    public AnimalCompaniaAdaptador crearAdaptador(ArrayList<AnimalCompania> animalesCompania) {
+        AnimalCompaniaAdaptador adaptador = new AnimalCompaniaAdaptador(animalesCompania, FavoritosActivity.this);
+        return adaptador;
     }
 }
